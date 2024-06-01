09dace699e7b0430692ca2e9ebb778a115319fb5
gitCiccio <ftartaglia98@gmail.com>
2024-06-01 16:42:36 +0200

    Aggiunta icona top bar e searchbar

--- app/src/main/java/com/example/caesarzonapplication/ui/components/AppTopBar.kt
+++ app/src/main/java/com/example/caesarzonapplication/ui/components/AppTopBar.kt
@@ -1,9 +1,14 @@
 package com.example.caesarzonapplication.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,21 +16,37 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.caesarzonapplication.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppTopBar(){
     var textFieldValue by remember { mutableStateOf("")}
 
-    TopAppBar(title = { Text(text = "Caesarzon") },
+    TopAppBar(title = {
+        TextField(
+            value = textFieldValue,
+            onValueChange = { textFieldValue = it },
+            modifier = Modifier
+                .padding(15.dp),
+            placeholder = { Text(text = "Cerca...")},
+            shape = RoundedCornerShape(50),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.LightGray,
+                focusedTextColor = Color.Black
+            )
+        )
+        },
         navigationIcon = {
-            TextField(
-                value = textFieldValue,
-                onValueChange = { textFieldValue = it },
+            Image(
+                painter = painterResource(id = R.drawable.mini),
+                contentDescription = "Caesarzon",
                 modifier = Modifier
-                    .padding(8.dp),
-                placeholder = { Text(text = "Cerca...")}
+                    .height(60.dp)
+                    .padding(vertical = 4.dp)
             )
         }
     )
