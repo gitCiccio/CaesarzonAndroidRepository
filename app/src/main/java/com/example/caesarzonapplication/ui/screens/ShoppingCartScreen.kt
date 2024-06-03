5f5d6e57167567a4c774c13e7c225872430cf283
gitCiccio <ftartaglia98@gmail.com>
2024-06-03 15:45:53 +0200

    Aggiunto carello

--- app/src/main/java/com/example/caesarzonapplication/ui/screens/ShoppingCartScreen.kt
+++ app/src/main/java/com/example/caesarzonapplication/ui/screens/ShoppingCartScreen.kt
@@ -1,12 +1,18 @@
 package com.example.caesarzonapplication.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -15,35 +21,54 @@ import com.example.caesarzonapplication.ui.components.EmptyShoppingCart
 import com.example.caesarzonapplication.ui.components.HorizontalProductSection
 import com.example.caesarzonapplication.ui.components.NavigationBottomBar
 import com.example.caesarzonapplication.ui.components.VerticalProductSection
+import com.example.caesarzonapplication.viewmodels.HomeViewModel
 import com.example.caesarzonapplication.viewmodels.ProductsViewModel
 
 @Composable
-fun ShoppingCartScreen(padding: PaddingValues, shoppingCartViewModel: ProductsViewModel) {
+fun ShoppingCartScreen(padding: PaddingValues, shoppingCartViewModel: ProductsViewModel, homeViewModel: HomeViewModel) {
     Scaffold(
         topBar = {},
-        bottomBar = { NavigationBottomBar(navController = rememberNavController())},
+        bottomBar = { NavigationBottomBar(navController = rememberNavController()) },
         content = { padding ->
-            Column (
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(padding)
                     .padding(top = 20.dp)
                     .background(Color(247, 170, 76, 255))
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
             ) {
-                if(shoppingCartViewModel.productInShoppingCart.isEmpty()){
-                    Column(modifier = Modifier
-                        .padding(100.dp)) {
+                if (shoppingCartViewModel.productInShoppingCart.isEmpty()) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
                         EmptyShoppingCart()
                     }
-                }else {
-                    Column(modifier = Modifier
-                        .padding(100.dp)){
-                        VerticalProductSection(title = "Prodotti", products = shoppingCartViewModel.productInShoppingCart)
+                } else {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        VerticalProductSection(
+                            title = "Prodotti",
+                            products = shoppingCartViewModel.productInShoppingCart
+                        )
                     }
                 }
-                HorizontalProductSection(title = "Altri prodotti", products = shoppingCartViewModel.productInShoppingCart)
+                Spacer(modifier = Modifier.height(20.dp))
+                HorizontalProductSection(
+                    title = "Altri prodotti",
+                    products = homeViewModel.products
+                )
             }
         }
-
     )
 }
