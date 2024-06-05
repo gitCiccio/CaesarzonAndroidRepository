a76ff919f23834931ce301dec0cf4b3a18c4bb92
Alberto Bifano <thegladiator2310@gmail.com>
2024-06-05 18:59:56 +0200

    Aggiornamento sezione lista amici

--- app/src/main/java/com/example/caesarzonapplication/ui/screens/FriendlistScreen.kt
+++ app/src/main/java/com/example/caesarzonapplication/ui/screens/FriendlistScreen.kt
@@ -5,18 +5,28 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -36,57 +46,128 @@ import androidx.navigation.compose.rememberNavController
 import com.example.caesarzonapplication.model.User
 import com.example.caesarzonapplication.ui.components.NavigationBottomBar
 import com.example.caesarzonapplication.viewmodels.UserViewModel
+import androidx.compose.runtime.mutableStateListOf
 
+
+enum class UsersTab {
+    Utenti,
+    Amici,
+    Preferiti
+}
 @Composable
 fun FriendlistScreen(userViewModel: UserViewModel= viewModel()) {
     var searchQuery by remember {
         mutableStateOf("")
     }
+    var selectedTab by remember { mutableStateOf(UsersTab.Utenti) }
 
-    Scaffold (
+    Scaffold(
         topBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.White)
-                    .padding(40.dp),
-                contentAlignment = Alignment.Center
-            )
-            {
-                Text(
-                    text = "La tua friendlist",
-                    style = TextStyle(
-                        fontSize = 28.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center
-                    ),
+            Column {
+                Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(20.dp)
-                        .background(Color.White),
-                    color = Color.Black,
+                        .background(Color.White)
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "La tua friendlist",
+                        style = TextStyle(
+                            fontSize = 28.sp,
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.White),
+                        color = Color.Black,
+                    )
+                }
+                TextField(
+                    value = searchQuery,
+                    onValueChange = { searchQuery = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    placeholder = { Text(text = "Cerca...") },
+                    shape = RoundedCornerShape(50),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.LightGray,
+                        focusedTextColor = Color.Black,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    )
                 )
             }
         },
-        bottomBar = { NavigationBottomBar(navController = rememberNavController())},
-        content = {padding ->
-            Column (
+        bottomBar = { NavigationBottomBar(navController = rememberNavController()) },
+        content = { padding ->
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(padding)
                     .background(Color.White)
-            ){
-                LazyColumn (
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(8.dp)
-                ){
-                    items(userViewModel.users.filter {
-                        it.username.contains(searchQuery, ignoreCase = true)
-                    }){
-                        user ->
-                        UserRow(remember { user }, userViewModel)
+            ) {
+                ScrollableTabRow(
+                    selectedTabIndex = selectedTab.ordinal,
+                    edgePadding = 0.dp,
+                    indicator = { tabPositions ->
+                        TabRowDefaults.Indicator(
+                            Modifier.tabIndicatorOffset(tabPositions[selectedTab.ordinal])
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    UsersTab.entries.forEach { tab ->
+                        Tab(
+                            text = { Text(text = tab.name) },
+                            selected = selectedTab == tab,
+                            onClick = { selectedTab = tab },
+                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                        )
                     }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                when (selectedTab) {
+                    UsersTab.Utenti ->
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(8.dp)
+                        ) {
+                            items(userViewModel.users.filter {
+                                it.username.contains(searchQuery, ignoreCase = true)
+                            }) { user ->
+                                UserRow(remember { user }, userViewModel)
+                            }
+                        }
+                    UsersTab.Amici ->
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(8.dp)
+                        ) {
+                            items(userViewModel.friends.filter {
+                                it.username.contains(searchQuery, ignoreCase = true)
+                            }) { user ->
+                                FriendsRow(remember { user }, userViewModel)
+                            }
+                        }
+                    UsersTab.Preferiti ->
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(8.dp)
+                        ) {
+                            items(userViewModel.favorites.filter {
+                                it.username.contains(searchQuery, ignoreCase = true)
+                            }) { user ->
+                                UserRow(remember { user }, userViewModel)
+                            }
+                        }
                 }
             }
         }
@@ -95,28 +176,75 @@ fun FriendlistScreen(userViewModel: UserViewModel= viewModel()) {
 
 @Composable
 fun UserRow(user: User, userViewModel: UserViewModel) {
+
     Row (
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
     ){
-        Text(text= user.username, modifier = Modifier
-            .padding(12.dp)
+        Text(
+            text = user.username,
+            modifier = Modifier
+                .padding(12.dp)
+                .weight(1f)
         )
         var userFavorite by rememberSaveable { mutableStateOf(user.isFavorite) }
-        if(userFavorite){
+        if (userFavorite) {
             Icon(
-                Icons.Filled.Favorite,
+                imageVector = Icons.Filled.Clear,
                 contentDescription = null,
-                modifier = Modifier.clickable { userFavorite=false; userViewModel.toggleFavorite(user) }
+                modifier = Modifier
+                    .clickable { userFavorite = false; userViewModel.toggleFavorite(user) }
+                    .padding(12.dp)
             )
-        }else{
+        } else {
             Icon(
-                Icons.Filled.FavoriteBorder,
+                imageVector = Icons.Filled.Add,
                 contentDescription = null,
-                modifier = Modifier.clickable { userFavorite=true; userViewModel.toggleFavorite(user) }
+                modifier = Modifier
+                    .clickable { userFavorite = true; userViewModel.toggleFavorite(user); userViewModel.addFriend(user)}
+                    .padding(12.dp)
             )
         }
     }
 }
+
+@Composable
+fun FriendsRow(user: User, userViewModel: UserViewModel) {
+
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = user.username,
+            modifier = Modifier
+                .padding(12.dp)
+                .weight(1f)
+        )
+        var userFavorite by rememberSaveable { mutableStateOf(user.isFavorite) }
+        if (userFavorite) {
+            Icon(
+                imageVector = Icons.Filled.Favorite,
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { userFavorite = false; userViewModel.toggleFavorite(user) }
+                    .padding(12.dp)
+            )
+        } else {
+            Icon(
+                imageVector = Icons.Filled.FavoriteBorder,
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { userFavorite = true; userViewModel.toggleFavorite(user) }
+                    .padding(12.dp)
+            )
+        }
+    }
+}
+
+
 
