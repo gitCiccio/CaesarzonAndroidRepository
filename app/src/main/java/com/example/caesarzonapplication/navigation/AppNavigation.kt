2a47870b645e323c9107c424b4569a6f3364adb8
CesareGiroldini <cgiroldini@gmail.com>
2024-06-30 15:06:09 +0200

    Modifica al WishlistViewModel + creata pagina di registrazione utente con navigazione

--- app/src/main/java/com/example/caesarzonapplication/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/caesarzonapplication/navigation/AppNavigation.kt
@@ -1,6 +1,5 @@
 package com.example.caesarzonapplication.navigation
 
-
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
